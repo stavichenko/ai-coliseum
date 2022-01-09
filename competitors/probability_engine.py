2e469cac2aca7f8580cb5abe9394185a113abf55
yaga <yadviha.khoshtaria@requestum.com>
2022-01-09 19:03:25 +0200

    ...

--- competitors/probability_engine.py
+++ competitors/probability_engine.py
@@ -3,14 +3,33 @@ import numpy as np
 
 
 class ProbabilityPlayer(AbstractPlayer):
+    def __init__(self):
+        self.field = np.array([AbstractPlayer.config["field_size"]])
+        self.opponent_field = np.array([AbstractPlayer.config["field_size"]])
+
     def get_setup(self):
-        return np.zeros([10, 10])
+        return self.field
 
     def get_move(self):
         pass
 
     def notify(self, x, y, status):
         pass
+
+    def bound_zeroes(self, ship: np.array):
+        x0 = min(ship[:, 0])
+        y0 = min(ship[:, 1])
+        x0 = x0 if (x0 - 1) < 0 else x0 - 1
+        y0 = y0 if (y0 - 1) < 0 else y0 - 1
+
+        x1 = max(ship[:, 0])
+        y1 = max(ship[:, 1])
+        x1 = x1 if (x1 + 1) > self.field.shape[0] - 1 else x1 + 1
+        y1 = y1 if (y1 + 1) > self.field.shape[0] - 1 else y1 + 1
+
+        for i in range(x0, x1 + 1):
+            for j in range(y0, y1 + 1):
+                self.field[i, j] = 0 if not self.field[i, j] else 1
 
 
 player = ProbabilityPlayer()
--- players/base.py
+++ players/base.py
@@ -12,3 +12,19 @@ class AbstractPlayer:
 
     def notify(self, x, y, status):
         raise NotImplementedError()
+
+
+class HumanSetupFieldPlayer:
+    config = {
+        "values": [[1, 4], [2, 3], [3, 2], [4, 1]],  # first value - count of cells, second value - count of ships
+        "field_side": 10
+    }
+
+    def get_setup(self):
+        raise NotImplementedError()
+
+    def get_move(self):
+        raise NotImplementedError()
+
+    def notify(self, x, y, status):
+        raise NotImplementedError()
