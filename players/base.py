038907ceae2a5e1c18a7afa2358c8c859b9cf07f
yaga <yadviha.khoshtaria@requestum.com>
2022-01-09 16:06:33 +0200

    ...

--- players/base.py
+++ players/base.py
@@ -1,8 +1,5 @@
 class AbstractPlayer:
-    config = {
-        "shape": "horizontal",
-        "values": [[1, 4], [2, 3], [3, 2], [4, 1]],
-    }
+    config = [[1, 4], [2, 3], [3, 2], [4, 1]]  # first value - count of cells, second value - count of ships
 
     def get_setup(self):
         raise NotImplementedError()
